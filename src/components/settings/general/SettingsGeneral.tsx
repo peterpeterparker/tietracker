22339190f2532752dd96fff0f1a0d6b606639961
peterpeterparker <david.dalbusco@outlook.com>
2019-12-22 14:23:59 +0100

    feat: extract general settings to own component

---
+++ src/components/settings/general/SettingsGeneral.tsx
@@ -0,0 +1,109 @@
+import React, {useEffect, useState} from 'react';
+
+import {Settings} from '../../../models/settings';
+import {
+    IonInput,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonSelect, IonSelectOption,
+} from '@ionic/react';
+import {Currencies, SettingsService} from '../../../services/settings/settings.service';
+
+export interface SettingsGeneralProps {
+    settings: Settings;
+}
+
+const SettingsGeneral: React.FC<SettingsGeneralProps> = (props) => {
+
+    const [currencies, setCurrencies] = useState<Currencies | undefined>(undefined);
+
+    useEffect(() => {
+        initCurrencies();
+    }, []);
+
+    async function initCurrencies() {
+        const currencies: Currencies | undefined = await SettingsService.getInstance().currencies();
+        setCurrencies(currencies);
+    }
+
+    function onCurrencyChange($event: CustomEvent) {
+        if (!$event || !$event.detail) {
+            return;
+        }
+
+        props.settings.currency = $event.detail.value;
+    }
+
+    function onRoundTimeChange($event: CustomEvent) {
+        if (!$event || !$event.detail) {
+            return;
+        }
+
+        props.settings.roundTime = $event.detail.value;
+    }
+
+    function onVatInput($event: CustomEvent<KeyboardEvent>) {
+        if (!$event) {
+            return;
+        }
+
+        const input: string = ($event.target as InputTargetEvent).value;
+
+        if (!input || input === undefined || input === '') {
+            delete props.settings['vat'];
+        } else {
+            props.settings.vat = parseFloat(($event.target as InputTargetEvent).value);
+        }
+    }
+
+    return (
+        <IonList className="inputs-list">
+            <IonItem className="item-title">
+                <IonLabel>Round time</IonLabel>
+            </IonItem>
+
+            <IonItem className="item-input">
+                <IonSelect placeholder="Round time" value={props.settings.roundTime} onIonChange={($event: CustomEvent) => onRoundTimeChange($event)}>
+                    <IonSelectOption value={1}>1 minute</IonSelectOption>
+                    <IonSelectOption value={5}>5 minutes</IonSelectOption>
+                    <IonSelectOption value={15}>15 minutes</IonSelectOption>
+                </IonSelect>
+            </IonItem>
+
+            <IonItem className="item-title">
+                <IonLabel>Currency</IonLabel>
+            </IonItem>
+
+            <IonItem className="item-input">
+                {renderCurrencies()}
+            </IonItem>
+
+            <IonItem className="item-title">
+                <IonLabel>Vat</IonLabel>
+            </IonItem>
+            <IonItem>
+                <IonInput debounce={500} input-mode="text" value={props.settings.vat ? `${props.settings.vat}` : ''}
+                          onIonInput={($event: CustomEvent<KeyboardEvent>) => onVatInput($event)}>
+                </IonInput>
+            </IonItem>
+        </IonList>
+    );
+
+    function renderCurrencies() {
+        if (!currencies || currencies === undefined) {
+            return undefined;
+        }
+
+        return <IonSelect placeholder="Currency" value={props.settings.currency} onIonChange={($event: CustomEvent) => onCurrencyChange($event)}>
+            {
+                Object.keys(currencies).map((key: string) => {
+                    return <IonSelectOption value={key} key={`currency-${key}`}>{currencies[key].name} ({key})</IonSelectOption>
+                })
+            }
+        </IonSelect>
+    }
+
+};
+
+export default SettingsGeneral;
--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -4,36 +4,25 @@ import {useSelector} from 'react-redux';
 import {
     IonContent,
     IonHeader,
-    IonItem,
     IonLabel,
-    IonList,
     IonPage,
     IonSpinner,
     IonTitle,
-    IonToolbar, useIonViewWillEnter,
-    IonSelect, IonSelectOption, IonButton, IonInput
+    IonToolbar,
+    IonButton,
 } from '@ionic/react';
-
-import styles from './Settings.module.scss';
 
 import {rootConnector, RootProps} from '../../store/thunks/index.thunks';
 import {RootState} from '../../store/reducers';
 
 import {Settings as SettingsModel} from '../../models/settings';
-import {Currencies, SettingsService} from '../../services/settings/settings.service';
+import SettingsGeneral from '../../components/settings/general/SettingsGeneral';
 
 const Settings: React.FC<RootProps> = (props) => {
 
     const settings: SettingsModel = useSelector((state: RootState) => state.settings.settings);
 
     const [saving, setSaving] = useState<boolean>(false);
-
-    const [currencies, setCurrencies] = useState<Currencies | undefined>(undefined);
-
-    useIonViewWillEnter(async () => {
-         const currencies: Currencies | undefined = await SettingsService.getInstance().currencies();
-         setCurrencies(currencies);
-    });
 
     async function handleSubmit($event: FormEvent<HTMLFormElement>) {
         $event.preventDefault();
@@ -48,36 +37,6 @@ const Settings: React.FC<RootProps> = (props) => {
         }
 
         setSaving(false);
-    }
-
-    function onCurrencyChange($event: CustomEvent) {
-        if (!$event || !$event.detail) {
-            return;
-        }
-
-        settings.currency = $event.detail.value;
-    }
-
-    function onRoundTimeChange($event: CustomEvent) {
-        if (!$event || !$event.detail) {
-            return;
-        }
-
-        settings.roundTime = $event.detail.value;
-    }
-
-    function onVatInput($event: CustomEvent<KeyboardEvent>) {
-        if (!$event) {
-            return;
-        }
-
-        const input: string = ($event.target as InputTargetEvent).value;
-
-        if (!input || input === undefined || input === '') {
-            delete settings['vat'];
-        } else {
-            settings.vat = parseFloat(($event.target as InputTargetEvent).value);
-        }
     }
 
     return (
@@ -99,55 +58,12 @@ const Settings: React.FC<RootProps> = (props) => {
         }
 
         return <form onSubmit={($event: FormEvent<HTMLFormElement>) => handleSubmit($event)}>
-            <IonList className="inputs-list">
-                <IonItem className="item-title">
-                    <IonLabel>Currency</IonLabel>
-                </IonItem>
-
-                <IonItem className="item-input">
-                    {renderCurrencies()}
-                </IonItem>
-
-                <IonItem className="item-title">
-                    <IonLabel>Round time</IonLabel>
-                </IonItem>
-
-                <IonItem className="item-input">
-                    <IonSelect placeholder="Round time" value={settings.roundTime} onIonChange={($event: CustomEvent) => onRoundTimeChange($event)}>
-                        <IonSelectOption value={1}>1 minute</IonSelectOption>
-                        <IonSelectOption value={5}>5 minutes</IonSelectOption>
-                        <IonSelectOption value={15}>15 minutes</IonSelectOption>
-                    </IonSelect>
-                </IonItem>
-
-                <IonItem className="item-title">
-                    <IonLabel>Vat</IonLabel>
-                </IonItem>
-                <IonItem>
-                    <IonInput debounce={500} input-mode="text" value={settings.vat ? `${settings.vat}` : ''}
-                              onIonInput={($event: CustomEvent<KeyboardEvent>) => onVatInput($event)}>
-                    </IonInput>
-                </IonItem>
-            </IonList>
+            <SettingsGeneral settings={settings}></SettingsGeneral>
 
             <IonButton type="submit" disabled={saving} aria-label="Update task">
                 <IonLabel>Save</IonLabel>
             </IonButton>
         </form>
-    }
-
-    function renderCurrencies() {
-        if (!currencies || currencies === undefined) {
-            return undefined;
-        }
-
-        return <IonSelect placeholder="Currency" value={settings.currency} onIonChange={($event: CustomEvent) => onCurrencyChange($event)}>
-            {
-                Object.keys(currencies).map((key: string) => {
-                    return <IonSelectOption value={key} key={`currency-${key}`}>{currencies[key].name} ({key})</IonSelectOption>
-                })
-            }
-        </IonSelect>
     }
 };
 
