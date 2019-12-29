4a2fea4fd1cf300c550ab77bfd82bca0dbed9e5a
peterpeterparker <david.dalbusco@outlook.com>
2019-12-29 15:59:22 +0100

    feat: resolve warning

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -18,7 +18,7 @@ const About: React.FC = () => {
                     <div className={styles.intro}>
                         <img src="assets/icon/logo.svg" alt="Tie Tracker logo" height="160"/>
 
-                        <p><IonLabel><strong>Tie Tracker</strong> - A simple, open source and free time tracking app ⏱️</IonLabel></p>
+                        <p><IonLabel><strong>Tie Tracker</strong> - A simple, open source and free time tracking app <span aria-label="stopwatch" role="img">⏱</span>️</IonLabel></p>
                     </div>
 
                     <p><IonLabel>Tie Tracker is a free and open source time tracking application. Its usage is meant to be simple and has the goal to lets you track work hours across projects.</IonLabel></p>
@@ -28,9 +28,9 @@ const About: React.FC = () => {
                     <p><IonLabel>Track your productivity and billable hours.</IonLabel></p>
 
                     <ul className={styles.list}>
-                        <li>✅ Simple work hours tracking</li>
-                        <li>✅ Assign time to clients and projects</li>
-                        <li>✅ Mark entries as billed</li>
+                        <li><span aria-label="checkmark" role="img" >✅</span> Simple work hours tracking</li>
+                        <li><span aria-label="checkmark" role="img">✅</span> Assign time to clients and projects</li>
+                        <li><span aria-label="checkmark" role="img">✅</span> Mark entries as billed</li>
                     </ul>
 
                     <h1 className="ion-padding-top">Reporting</h1>
@@ -38,46 +38,46 @@ const About: React.FC = () => {
                     <p><IonLabel>Yet not complicated but effective reporting.</IonLabel></p>
 
                     <ul className={styles.list}>
-                        <li>✅ Weekly work summary</li>
-                        <li>✅ Today's list of activities</li>
-                        <li>✅ Export open invoices to CSV timesheets</li>
+                        <li><span aria-label="checkmark" role="img">✅</span> Weekly work summary</li>
+                        <li><span aria-label="checkmark" role="img">✅</span> Today's list of activities</li>
+                        <li><span aria-label="checkmark" role="img">✅</span> Export open invoices to CSV timesheets</li>
                     </ul>
 
                     <h1 className="ion-padding-top">Open Source</h1>
 
-                    <p><IonLabel>Tie Tracker is open source and published under the AGPL v3 (or later) licence. Its code is available on <a href="http://github.com/peterpeterparker/tietracker" target="_blank" className={styles.github}><IonIcon icon={logoGithub} area-label="Github"></IonIcon> GitHub</a>.</IonLabel></p>
+                    <p><IonLabel>Tie Tracker is open source and published under the AGPL v3 (or later) licence. Its code is available on <a href="http://github.com/peterpeterparker/tietracker" target="_blank" rel="noopener noreferrer" className={styles.github}><IonIcon icon={logoGithub} area-label="Github"></IonIcon> GitHub</a>.</IonLabel></p>
 
                     <h1 className="ion-padding-top">Author</h1>
 
-                    <p><IonLabel>I, David Dal Busco, am the developer behind this application. I am a freelancer, full stack developer Web, PWA and Mobile iOS/Android, UX and IT consultant by day and the creator of <a href="https://deckdeckgo.com" target="_black">DeckDeckGo</a> by night.</IonLabel></p>
+                    <p><IonLabel>I, David Dal Busco, am the developer behind this application. I am a freelancer, full stack developer Web, PWA and Mobile iOS/Android, UX and IT consultant by day and the creator of <a href="https://deckdeckgo.com" target="_blank" rel="noopener noreferrer">DeckDeckGo</a> by night.</IonLabel></p>
 
-                    <p className="ion-padding-top"><IonLabel>This app is developed with <a href="https://ionicframework.com" target="_blank">Ionic</a>, <a href="https://reactjs.org" target="_blank">React</a>, <a href="https://react-redux.js.org" target="_blank">Redux</a>, Web Workers and other cool stuffs.</IonLabel></p>
+                    <p className="ion-padding-top"><IonLabel>This app is developed with <a href="https://ionicframework.com" target="_blank" rel="noopener noreferrer">Ionic</a>, <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a>, <a href="https://react-redux.js.org" target="_blank" rel="noopener noreferrer">Redux</a>, Web Workers and other cool stuffs.</IonLabel></p>
 
                     <p className="ion-padding-top"><IonLabel>To contact me, find some of my blog posts, browse other works or maybe even to hire me, follow these links.</IonLabel></p>
 
                     <div className={styles.social}>
-                        <a href="https://twitter.com/daviddalbusco" target="_blank">
+                        <a href="https://twitter.com/daviddalbusco" target="_blank" rel="noopener noreferrer">
                             <IonIcon icon={logoTwitter} area-label="Twitter"></IonIcon>
                         </a>
 
-                        <a href="https://daviddalbusco.com" target="_blank">
+                        <a href="https://daviddalbusco.com" target="_blank" rel="noopener noreferrer">
                             <IonIcon icon={globe} area-label="Personal blog and website"></IonIcon>
                         </a>
 
-                        <a href="https://dev.to/daviddalbusco" target="_blank">
+                        <a href="https://dev.to/daviddalbusco" target="_blank" rel="noopener noreferrer">
                             <IonIcon src="./assets/icon/dev.svg" area-label="Dev"></IonIcon>
                         </a>
 
-                        <a href="https://medium.com/@david.dalbusco" target="_blank">
+                        <a href="https://medium.com/@david.dalbusco" target="_blank" rel="noopener noreferrer">
                             <IonIcon src="./assets/icon/medium.svg" area-label="Medium"></IonIcon>
                         </a>
 
-                        <a href="http://github.com/peterpeterparker" target="_blank">
+                        <a href="http://github.com/peterpeterparker" target="_blank" rel="noopener noreferrer">
                             <IonIcon icon={logoGithub} area-label="Github"></IonIcon>
                         </a>
                     </div>
 
-                    <p className="ion-padding-top"><IonLabel>To infinity and beyond 🚀</IonLabel></p>
+                    <p className="ion-padding-top"><IonLabel>To infinity and beyond <span role="img" aria-label="rocket">🚀</span></IonLabel></p>
 
                     <p><IonLabel>David</IonLabel></p>
                 </main>
