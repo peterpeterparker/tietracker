44f7c22d109a6046e81969153768ef55554cffe1
peterpeterparker <david.dalbusco@outlook.com>
2019-12-29 09:23:55 +0100

    feat: try native API

--- src/services/export/export.service.ts
+++ src/services/export/export.service.ts
@@ -18,21 +18,71 @@ export class ExportService {
     }
 
     export(projectId: string, from: Date | undefined, to: Date | undefined, currency: string): Promise<void> {
-        return new Promise<void>((resolve) => {
+        return new Promise<void>(async (resolve, reject) => {
             const invoices: string[] | undefined = interval(from, to);
 
             if (invoices === undefined) {
-                resolve();
+                reject('No invoices to export.');
                 return;
             }
 
-            this.exportWorker.onmessage = ($event: MessageEvent) => {
-                console.log($event);
-            };
+            try {
+                const fileHandle: FileSystemFileHandle = await this.getNewFileHandle();
 
-            this.exportWorker.postMessage({msg: 'export', invoices: invoices, projectId: projectId, currency: currency});
+                if (!fileHandle) {
+                    reject('Cannot access filesystem.');
+                    return;
+                }
 
-            resolve();
+                const writer = await fileHandle.createWriter();
+
+                // TODO
+
+                this.exportWorker.onmessage = async ($event: MessageEvent) => {
+                    console.log(writer);
+
+                    if ($event && $event.data) {
+                        await writer.write(0, $event.data);
+                    } else {
+                        await writer.close();
+                    }
+
+                };
+
+                this.exportWorker.postMessage({
+                    msg: 'export',
+                    invoices: invoices,
+                    projectId: projectId,
+                    currency: currency
+                });
+
+                resolve();
+            } catch (err) {
+                console.error(err);
+                reject(err);
+            }
         });
+    }
+
+    private async getNewFileHandle(): Promise<FileSystemFileHandle> {
+        const opts: ChooseFileSystemEntriesOptions = {
+            type: 'save-file',
+            accepts: [{
+                description: 'Export',
+                extensions: ['csv'],
+                mimeTypes: ['text/csv'],
+            }],
+        };
+
+        return chooseFileSystemEntries(opts);
+    }
+
+    private async writeFile(fileHandle: FileSystemFileHandle, contents: string | BufferSource | Blob) {
+        // Create a writer (request permission if necessary).
+        const writer = await fileHandle.createWriter();
+        // Write the full length of the contents
+        await writer.write(0, contents);
+        // Close the file and write the contents to disk
+        await writer.close();
     }
 }
