6ca2acfe1459fd028dcb5e3d043ed1bc71a93a78
peterpeterparker <david.dalbusco@outlook.com>
2021-06-26 18:52:10 +0200

    feat: redo hide splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import {Translation} from 'react-i18next';
 
 import {options, card, information, statsChart} from 'ionicons/icons';
 
+import {SplashScreen} from '@capacitor/splash-screen';
+
 import Settings from './pages/settings/Settings';
 import Invoices from './pages/invoices/Invoices';
 import Statistics from './pages/statistics/Statistics';
@@ -75,6 +77,8 @@ const App: React.FC<RootProps> = (props: RootProps) => {
   async function init() {
     // Init theme first
     await props.initTheme();
+
+    await SplashScreen.hide();
 
     await initAllData(props);
   }
