fb385cae81bf19f6e5c96aa4f176b2d868ef2c07
David Dal Busco <david.dalbusco@outlook.com>
2026-02-01 07:42:31 +0100

    build: use dist folder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type {CapacitorConfig} from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tietracker.app',
   appName: 'Tie Tracker',
-  webDir: 'build',
+  webDir: 'dist',
   cordova: {},
   plugins: {
     SplashScreen: {
