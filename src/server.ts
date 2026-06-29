71b5fa96302a602a163565bfff68c27dcc69fc34
David Dal Busco <david.dalbusco@outlook.com>
2026-06-29 12:33:30 +0200

    feat: kyushu prebuild script and simplified ASSETS

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,6 @@ import type {ExportedHandler} from 'kyushu-types';
 
 export default {
   async fetch(request, env) {
-    const response = await env.ASSETS?.fetch(request);
-
-    if (response === undefined) {
-      return {status: 500, body: 'Internal Server Error'};
-    }
-
-    return response;
+    return await env.ASSETS.fetch(request);
   },
 } satisfies ExportedHandler;
