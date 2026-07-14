ff9895af6304b10d438d96d3c866a32c9db7ed55
David Dal Busco <david.dalbusco@outlook.com>
2026-07-14 07:13:58 +0200

    chore: remove deprecated option

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import {defineConfig} from 'vite';
 
 export default defineConfig({
   plugins: [react(), legacy()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-      },
-    },
-  },
 });
