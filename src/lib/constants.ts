6a5a084e57f6c2e634d25aa6a61b664dbe04c3b3
David Dal Busco <david.dalbusco@outlook.com>
2026-07-16 17:35:30 +0200

    feat: backup and restore preferences (#282)
    * feat: backup and restore preferences
    
    * feat: simplify

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,4 @@
 export const CLIENT_COLOR_FALLBACK = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+// TODO: extract key constants
+export const PREFERENCES_KEYS = ['backup', 'dark_mode', 'settings'];
