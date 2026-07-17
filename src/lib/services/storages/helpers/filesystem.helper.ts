2eba598efc7bbb2e308b280487c3ef81d1ea92d2
David Dal Busco <david.dalbusco@outlook.com>
2026-07-17 09:22:45 +0200

    refactor: extract fs helpers

---
+++ src/lib/services/storages/helpers/filesystem.helper.ts
@@ -0,0 +1,143 @@
+import {
+  Directory,
+  Encoding,
+  type FileInfo,
+  Filesystem,
+  PluginError,
+  type ReadFileOptions,
+  type WriteFileOptions,
+} from '@capacitor/filesystem';
+import {isNotNativePlatform} from '../../../env';
+import {nonNullish} from '../../../utils/utils.nullish';
+
+// TODO: option Directory.Library or Directory.LibraryNoCloud
+const DIRECTORY: Directory = Directory.Library;
+
+const ENCODING: Pick<ReadFileOptions, 'encoding'> | Pick<WriteFileOptions, 'encoding'> = {
+  encoding: Encoding.UTF8,
+};
+
+const STORAGE_DIR_PATH = '__storage__';
+
+const filePath = (key: string): string => `${STORAGE_DIR_PATH}/${key}.json`;
+
+// https://capacitorjs.com/docs/apis/filesystem#errors
+const FILE_NOT_FOUND_ERROR_CODE = 'OS-PLUG-FILE-0008';
+const DIRECTORY_ALREADY_EXISTS_ERROR_CODE = 'OS-PLUG-FILE-0010';
+
+const isPluginError = (err: unknown): err is PluginError =>
+  nonNullish(err) &&
+  typeof err === 'object' &&
+  'code' in err &&
+  typeof (err as PluginError).code === 'string';
+
+const isNotFoundError = (err: unknown): boolean =>
+  isNotNativePlatform() || (isPluginError(err) && err.code === FILE_NOT_FOUND_ERROR_CODE);
+
+const isDirectoryAlreadyExistsError = (err: unknown): boolean =>
+  isNotNativePlatform() || (isPluginError(err) && err.code === DIRECTORY_ALREADY_EXISTS_ERROR_CODE);
+
+export const get = async <T>({key}: {key: string}): Promise<Option<T>> => {
+  try {
+    const {data} = await Filesystem.readFile({
+      ...ENCODING,
+      path: filePath(key),
+      directory: DIRECTORY,
+    });
+
+    const text = data instanceof Blob ? await data.text() : data;
+
+    return JSON.parse(text) as T;
+  } catch (err: unknown) {
+    if (isNotFoundError(err)) {
+      return undefined;
+    }
+
+    throw err;
+  }
+};
+
+export const ensureDataDirExists = async (): Promise<void> => {
+  const createDataDir = async () => {
+    try {
+      await Filesystem.mkdir({
+        path: STORAGE_DIR_PATH,
+        directory: DIRECTORY,
+        recursive: true,
+      });
+    } catch (err: unknown) {
+      if (isDirectoryAlreadyExistsError(err)) {
+        return;
+      }
+
+      throw err;
+    }
+  };
+
+  try {
+    await Filesystem.stat({
+      path: STORAGE_DIR_PATH,
+      directory: DIRECTORY,
+    });
+  } catch {
+    // stat rejects if the path does not exist yet.
+    await createDataDir();
+  }
+};
+
+export const set = async ({key, value}: {key: string; value: unknown}): Promise<void> => {
+  await ensureDataDirExists();
+
+  await Filesystem.writeFile({
+    ...ENCODING,
+    path: filePath(key),
+    directory: DIRECTORY,
+    data: JSON.stringify(value),
+  });
+};
+
+export const listFiles = async (): Promise<{files: FileInfo[]}> => {
+  try {
+    return await Filesystem.readdir({
+      path: STORAGE_DIR_PATH,
+      directory: DIRECTORY,
+    });
+  } catch (err: unknown) {
+    if (isNotFoundError(err)) {
+      return {files: []};
+    }
+
+    throw err;
+  }
+};
+
+export const clear = async (): Promise<void> => {
+  try {
+    await Filesystem.rmdir({
+      path: STORAGE_DIR_PATH,
+      directory: DIRECTORY,
+      recursive: true,
+    });
+  } catch (err: unknown) {
+    if (isNotFoundError(err)) {
+      return;
+    }
+
+    throw err;
+  }
+};
+
+export const del = async ({key}: {key: string}): Promise<void> => {
+  try {
+    await Filesystem.deleteFile({
+      path: filePath(key),
+      directory: DIRECTORY,
+    });
+  } catch (err: unknown) {
+    if (isNotFoundError(err)) {
+      return;
+    }
+
+    throw err;
+  }
+};
