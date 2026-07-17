80d6044bdfb80f849428dbfe4cc07bb9e446ec9b
David Dal Busco <david.dalbusco@outlook.com>
2026-07-17 09:19:54 +0200

    feat: filesystem storage directory as option

--- src/lib/services/storages/filesystem.storage.ts
+++ src/lib/services/storages/filesystem.storage.ts
@@ -1,127 +1,27 @@
-import {
-  Directory,
-  Encoding,
-  type FileInfo,
-  Filesystem,
-  PluginError,
-  type ReadFileOptions,
-  type WriteFileOptions,
-} from '@capacitor/filesystem';
-import {isNotNativePlatform} from '../../env';
-import {nonNullish} from '../../utils/utils.nullish';
+import {Directory} from '@capacitor/filesystem';
+import {clear, del, get, listFiles, set} from './helpers/filesystem.helper';
 import {KeyedStorage, Storage} from './storage';
 
-// TODO: option Directory.Library or Directory.LibraryNoCloud
-const DIRECTORY: Directory = Directory.Library;
-
-const ENCODING: Pick<ReadFileOptions, 'encoding'> | Pick<WriteFileOptions, 'encoding'> = {
-  encoding: Encoding.UTF8,
-};
-
-const STORAGE_DIR_PATH = '__storage__';
-
-const filePath = (key: string): string => `${STORAGE_DIR_PATH}/${key}.json`;
-
-// https://capacitorjs.com/docs/apis/filesystem#errors
-const FILE_NOT_FOUND_ERROR_CODE = 'OS-PLUG-FILE-0008';
-const DIRECTORY_ALREADY_EXISTS_ERROR_CODE = 'OS-PLUG-FILE-0010';
-
-const isPluginError = (err: unknown): err is PluginError =>
-  nonNullish(err) &&
-  typeof err === 'object' &&
-  'code' in err &&
-  typeof (err as PluginError).code === 'string';
-
-const isNotFoundError = (err: unknown): boolean =>
-  isNotNativePlatform() || (isPluginError(err) && err.code === FILE_NOT_FOUND_ERROR_CODE);
-
-const isDirectoryAlreadyExistsError = (err: unknown): boolean =>
-  isNotNativePlatform() || (isPluginError(err) && err.code === DIRECTORY_ALREADY_EXISTS_ERROR_CODE);
-
-const get = async <T>(key: string): Promise<Option<T>> => {
-  try {
-    const {data} = await Filesystem.readFile({
-      ...ENCODING,
-      path: filePath(key),
-      directory: DIRECTORY,
-    });
-
-    const text = data instanceof Blob ? await data.text() : data;
-
-    return JSON.parse(text) as T;
-  } catch (err: unknown) {
-    if (isNotFoundError(err)) {
-      return undefined;
-    }
-
-    throw err;
-  }
-};
-
-const ensureDataDirExists = async (): Promise<void> => {
-  const createDataDir = async () => {
-    try {
-      await Filesystem.mkdir({
-        path: STORAGE_DIR_PATH,
-        directory: DIRECTORY,
-        recursive: true,
-      });
-    } catch (err: unknown) {
-      if (isDirectoryAlreadyExistsError(err)) {
-        return;
-      }
-
-      throw err;
-    }
-  };
-
-  try {
-    await Filesystem.stat({
-      path: STORAGE_DIR_PATH,
-      directory: DIRECTORY,
-    });
-  } catch {
-    // stat rejects if the path does not exist yet.
-    await createDataDir();
-  }
-};
-
-const set = async (key: string, value: unknown): Promise<void> => {
-  await ensureDataDirExists();
-
-  await Filesystem.writeFile({
-    ...ENCODING,
-    path: filePath(key),
-    directory: DIRECTORY,
-    data: JSON.stringify(value),
-  });
-};
-
-const listFiles = async (): Promise<{files: FileInfo[]}> => {
-  try {
-    return await Filesystem.readdir({
-      path: STORAGE_DIR_PATH,
-      directory: DIRECTORY,
-    });
-  } catch (err: unknown) {
-    if (isNotFoundError(err)) {
-      return {files: []};
-    }
-
-    throw err;
-  }
-};
+const DEFAULT_DIRECTORY: Directory = Directory.Library;
 
 export class FilesystemStorage extends Storage {
+  readonly #directory: Directory;
+
+  constructor({directory}: {directory: Directory} = {directory: DEFAULT_DIRECTORY}) {
+    super();
+
+    this.#directory = directory;
+  }
+
   override async entries(): Promise<[string, unknown][]> {
-    const {files} = await listFiles();
+    const {files} = await listFiles(this.directory());
 
     const jsonFiles = files.filter((file) => file.name.endsWith('.json'));
 
     const entries = await Promise.all(
       jsonFiles.map(async (file): Promise<[string, unknown]> => {
         const key = file.name.replace(/\.json$/, '');
-        const value = await get(key);
+        const value = await get({key, ...this.directory()});
         return [key, value];
       }),
     );
@@ -130,50 +30,43 @@ export class FilesystemStorage extends Storage {
   }
 
   override async clear(): Promise<void> {
-    try {
-      await Filesystem.rmdir({
-        path: STORAGE_DIR_PATH,
-        directory: DIRECTORY,
-        recursive: true,
-      });
-    } catch (err: unknown) {
-      if (isNotFoundError(err)) {
-        return;
-      }
-
-      throw err;
-    }
+    await clear(this.directory());
   }
 
   override async setMany(entries: [string, unknown][]): Promise<void> {
     // Sequential set instead of promises to reduce stress in case the target directory has to be created
     for (const [key, value] of entries) {
-      await set(key, value);
+      await set({key, value, ...this.directory()});
     }
+  }
+
+  private directory(): {directory: Directory} {
+    return {directory: this.#directory};
   }
 }
 
 export class KeyedFilesystemStorage<T> extends KeyedStorage<T> {
+  readonly #directory: Directory;
+
+  constructor({key, directory = DEFAULT_DIRECTORY}: {key: string; directory?: Directory}) {
+    super({key});
+
+    this.#directory = directory;
+  }
+
   override get(): Promise<Option<T>> {
-    return get<T>(this.key);
+    return get<T>({key: this.key, ...this.directory()});
   }
 
   override async set(value: T): Promise<void> {
-    await set(this.key, value);
+    await set({key: this.key, value, ...this.directory()});
   }
 
   override async del(): Promise<void> {
-    try {
-      await Filesystem.deleteFile({
-        path: filePath(this.key),
-        directory: DIRECTORY,
-      });
-    } catch (err: unknown) {
-      if (isNotFoundError(err)) {
-        return;
-      }
+    await del({key: this.key, ...this.directory()});
+  }
 
-      throw err;
-    }
+  private directory(): {directory: Directory} {
+    return {directory: this.#directory};
   }
 }
