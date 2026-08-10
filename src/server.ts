0010253c8e0499320a9e22e8e4e1810eacb24192
David Dal Busco <david.dalbusco@outlook.com>
2026-08-10 14:55:46 +0200

    fix: ionic react spa and not mpa anymore

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,34 @@ import type {ExportedHandler} from 'kyushu-types';
 
 export default {
   async fetch(request, env) {
-    return await env.ASSETS.fetch(request);
+    const fetchWithSpaFallback = async () => {
+      const {url: requestUrl} = request;
+
+      const url = URL.parse(requestUrl);
+
+      if (url === null) {
+        return {status: 400, body: 'Bad Request'};
+      }
+
+      const {pathname} = url;
+
+      const hasNoExtension = !pathname.split('/').pop()?.includes('.');
+      const endsWithSlash = pathname.endsWith('/');
+
+      // SPA - Ionic React somehow does not generate a multi-page app anymore
+      if (hasNoExtension || endsWithSlash) {
+        // Imperative for simplicity and perf reasons
+        url.pathname = '/';
+
+        return await env.ASSETS.fetch({
+          ...request,
+          url: url.toString(),
+        });
+      }
+
+      return await env.ASSETS.fetch(request);
+    };
+
+    return await fetchWithSpaFallback();
   },
 } satisfies ExportedHandler;
